416d7ae103b66778a9f18ae6ad698875e55c2637
Teodora Sofilca <teodoras1908@gmail.com>
2022-04-07 18:52:47 +0300

    modificari tema 2

--- HW2/main.py
+++ HW2/main.py
@@ -1,11 +1,13 @@
 import mymodule
 
+
 def my_func(*args, **kwargs):
     sum = 0
     for arg in args:
         if type(arg) == int:
             sum += arg
     print(sum)
+
 
 print("3 examples of my_func")
 my_func(1, 5, -3, 'abc', [12, 56, 'cad'])
@@ -15,18 +17,13 @@ my_func(2, 4, 'abc', param_1=2)
 print('----------------------------------------------------------')
 
 
-def isint_func(*args, **kwargs):
-    param_1 = input("Please enter an integer: ")
-    if type(param_1) == int:
-        print(param_1)
-    elif type(param_1) != int:
-        print('0')
-    print(type(param_1)) # input() vine cu type de str, asadar niciun numar introdus de la tastatura nu va fi int
-    # decat daca il vom  converti la int
+def isint_func():
+    try:
+        param_1 = int(input("Please enter an integer: "))
+    except ValueError:
+        return 0
+    else:
+        return param_1
 
 
-isint_func()
-
-
-
-
+print(isint_func())
